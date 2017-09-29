41fe5492b3babe4c3298ac111cbb3081b7d4f852
dfawess <31991330+dfawess@users.noreply.github.com>
2017-09-29 15:55:00 +0300

    tekain

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $app=new\atk4\ui\App('Meincraft');
 $app->initLayout('Centered');
 $button = $app->add ('Button');
 $button->set ('report');
-$button->icon = 'plus';
+$button->icon = 'bug';
 $button->link('https://www.steamcommunity.com');
