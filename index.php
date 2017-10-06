68603bdf2ae8d33abe92941c8ee0bee0654b3560
dfawess <31991330+dfawess@users.noreply.github.com>
2017-10-06 14:07:07 +0300

    lol

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ $button = $app->add ('Button');
 $button->set ('report');
 $button->icon = 'bug';
 $button->link('https://www.steamcommunity.com');
-$button = $app->add ('Button');
-$button->set ('watch');
-$button->link('https://www.youtube.com');
+$button1 = $app->add ('Button');
+$button1->set ('watch');
+$button1->link('https://www.youtube.com');
+$button2 = $app->add ('Button');
+$button2->set ('diiiimooooon');
+$button2->icon = 'Heartbeat';
+$button2->addClass ('massive blue')
+$button2->link('https://www.e-klase.lv')
