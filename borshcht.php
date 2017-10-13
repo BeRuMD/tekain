34a86124a24500c285b8217d5a414f319fe265f9
dfawess <31991330+dfawess@users.noreply.github.com>
2017-10-13 15:58:32 +0300

    teakin

--- borshcht.php
+++ borshcht.php
@@ -4,3 +4,5 @@ $app=new \atk4\ui\App('GerojamSlava');
 $app->initLayout('Centered');
 
 $label1 = $app->add(['Label', 'maidan',' massive blue','icon'=>'gerojam slava','detail'=>'poshli zech pokrishka na maidan']);
+$img = 'https://evreimir.com/wp-content/uploads/2017/05/KNDR-702x336.jpg';
+$icon = $app->add(['Image',$img]);
