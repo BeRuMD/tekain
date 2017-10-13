4a8395e5ad5563e0548f693e0ca1784342fa660c
dfawess <31991330+dfawess@users.noreply.github.com>
2017-10-13 15:43:52 +0300

    xteakin

--- borshcht.php
+++ borshcht.php
@@ -3,4 +3,4 @@ require 'vendor/autoload.php';
 $app=new \atk4\ui\App('GerojamSlava');
 $app->initLayout('Centered');
 
-$label1 = $app->add(['Label', 'maidan',' massive blue','icon'=>'gerojam slava']);
+$label1 = $app->add(['Label', 'maidan',' massive blue','icon'=>'gerojam slava','detail'=>'poshli zech pokrish na maidan']);
