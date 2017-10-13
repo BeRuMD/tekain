510e061ff7f0e734f6bf2867baef99ccd7b10e0f
dfawess <31991330+dfawess@users.noreply.github.com>
2017-10-13 15:21:27 +0300

    tekain

--- borshcht.php
+++ borshcht.php
@@ -1,4 +1,5 @@
 <?php
 require 'vendor/autoload.php';
-$app=new\atk4\ui\App('Meincraft');
+$app=new \atk4\ui\App('Meincraft');
 $app->initLayout('Centered');
+$label = $app->add(['Label', 'maidan','icon'=>'gerojam slava']);
