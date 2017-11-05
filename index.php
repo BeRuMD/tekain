ec616e7ba98fb827281d800659cd7bafb52b6ac2
BeRuMD <31989247+BeRuMD@users.noreply.github.com>
2017-11-05 20:40:50 +0200

    sdrghdrgth
    rteyytjfghj

--- index.php
+++ index.php
@@ -1,30 +1,8 @@
 <?php
 require 'vendor/autoload.php';
-$app=new \atk4\ui\App('SlavaUkraine');
+$app = new \atk4\ui\App ('Gamer');
 $app->initLayout('Centered');
-/*$button = $app->add ('Button');
-$button->set ('report');
-$button->icon = 'bug';
-$button->link('https://www.steamcommunity.com');
+$button = $app->add (['Button','gaish']);
+$button -> icon = 'find';
 
-$button1 = $app->add ('Button');
-$button1->set ('watch');
-$button1->link('https://www.youtube.com');
-
-$button2 = $app->add ('Button');
-$button2->set ('diiiimooooon');
-$button2->icon = 'Heartbeat';
-$button2->addClass ('massive blue');
-$button2->link('https://www.e-klase.lv');
-$button2 = $app->add(['Button','Kurvish','iconRight'=>'right arrow']);
-$button3->addClass ('green');
-
-$bar = $app->add(['ui'=>'vertical buttons']);
-$bar->add(['Button', 'lok', 'icon'=>'lok']);
-$bar->add(['Button','puk','icon'=>'puk']);
-$bar->add(['Button','kol','icon'=>'kol');
-$bar->add(['Button','kndr','icon'=>'kndr']);
-*/
-$button = $app->add('Button');
-$button->set ('bot');
-$button -> link('borshcht.php');
+$button -> addClass('big red');
